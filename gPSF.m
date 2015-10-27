87a3b955d6618e6dcc520b09446a615173818f4b
Danilo A. Jesus <danilo.jesus@pwr.edu.pl>
2015-10-27 16:12:36 +0100

    Update and rename gPSF to gPSF.m

--- gPSF.m
+++ gPSF.m
@@ -5,13 +5,15 @@ function [] = gPSF(Data,NoR)
         NoR=25;             %Number of rays by default
     end
     
-    Data=Data{1,end-2};     %Intersection with the last surface
-    D=domain(Data);         %Domain
-    spc=180/size(Data,2);   %Rotational angle
+    ia=Data{end}(1);   %Rotational angle
+    spc=Data{end}(2);   %Rotational angle   
+    
+    Ints=Data{1,end-2};     %Intersection with the last surface
+    D=domain(Ints);         %Domain
     
     figure,hold on
-    for k=0:size(Data,2)-1; %for each meridian
-    S=Data(:,k+1);          %Data at meridian k          
+    for k=0:size(Ints,2)-1; %for each meridian
+    S=Ints(:,k+1);          %Data at meridian k          
     th=k*spc;               %Angle
     X=chebfun(@(x) real(S(x))*cos(th*pi/180)-imag(S(x))*sin(th*pi/180),D); 
     Y=chebfun(@(x) real(S(x))*sin(th*pi/180)+imag(S(x))*cos(th*pi/180),D);
